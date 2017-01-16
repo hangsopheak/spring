c30de9a9c71ea2e76a06c9b77b3f3dbca998b4b4
Sophea Mak <sm@goldengekko.com>
2017-01-16 23:25:23 +0700

    spring rest-apis sample

--- src/main/java/com/rupp/spring/dao/EmployeeDAO.java
+++
@@ -1,7 +0,0 @@
-package com.rupp.spring.dao;
-
-import com.rupp.spring.domain.EmployeeDTO;
-
-public interface EmployeeDAO {
-    EmployeeDTO createNewEmployee();
-}
--- src/main/java/com/rupp/spring/service/employee/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.rupp.spring.service.employee;
-
-import com.rupp.spring.domain.EmployeeDTO;
-
-public interface EmployeeService {
-    EmployeeDTO createNewEmployee();
-}
